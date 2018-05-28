e29b9ef7857a4db257ac7aa89716edc0eb253ad2
chenyeah <yuyehack@gmail.com>
2018-05-28 10:04:52 +0800

    add gitment test

---
+++ pages/test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div ref="comments">
+
+  </div>
+</template>
+<script>
+import "gitment/style/default.css";
+import Gitment from "gitment";
+export default {
+  data() {},
+  mounted() {
+    const gitment = new Gitment({
+      id: location.href, // optional
+      owner: "chenyeah",
+      repo: "chenyeah",
+      oauth: {
+        client_id: "11fa4735df6563f2e9c2",
+        client_secret: "090bc3eeb2c0201fc159778fa067e6df1eb8a17b"
+      }
+      // ...
+      // For more available options, check out the documentation below
+    });
+    gitment.render(this.$refs.comments);
+  }
+};
+</script>
