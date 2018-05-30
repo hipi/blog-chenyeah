69ad28e58e7c060ab94a38b197bc82390b2b0a32
chenyeah <yuyehack@gmail.com>
2018-05-30 21:45:43 +0800

    Fixed img width

--- components/ad.vue
+++ components/ad.vue
@@ -10,7 +10,7 @@
     box-shadow: 0 0 0 1px rgba(0, 0, 0, .02), 0 4px 10px rgba(0, 0, 0, .06);
     img{
         display: block;
-        max-width: 100%;
+        width: 100%;
         border-radius: 10px;
     }
 }
