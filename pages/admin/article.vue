c6b4ecc5cb312b6ec244bbad2e6fc773dfd57550
chenyeah <yuyehack@gmail.com>
2018-05-30 13:45:01 +0800

    Fixed

--- pages/admin/article.vue
+++ pages/admin/article.vue
@@ -22,7 +22,10 @@
           <img ref="articleImg" :class="[n.imgIsBroken?'broken-img':'']" :src="n.cover"> {{n.info}}
         </a>
         <div class="meta">
+          <i class="iconfont icon-activity"></i>
           <span>{{n.add_time}}</span>
+          <i v-if="n.modify_time" class="iconfont icon-brush"></i>
+          <span v-if="n.modify_time"> {{n.modify_time}}</span>
           <nuxt-link :to="`/blog/archives/${n.tag}`" v-if="n.tag" class="tag">
             <i class="iconfont icon-label"></i>{{n.tag}}
           </nuxt-link>
@@ -35,202 +38,193 @@
 <script>
 import axios from "axios";
 export default {
-    layout: "admin",
-    async asyncData({ query }) {
-        function getArticle() {
-            return axios
-                .post(
-                    "https://api.yuyehack.cn/blog/article/get_articlelist.php",
-                    {
-                        s: query.s,
-                        page: 1,
-                        pageSize: 8
-                    }
-                )
-                .then(res => {
-                    return {
-                        list: res.data.list,
-                        totalPages: res.data.totalPages
-                    };
-                })
-                .catch(e => {
-                    // context.error({ statusCode: 404, message: "出错啦" });
-                    return { list: [{ title: "出错啦", info: "请检查接口" }] };
-                });
-        }
-        let data = await getArticle();
-        return data;
-    },
-    data() {
-        return {
-            currentPage: 1,
-            isLoad: true
-        };
-    },
-
-    computed: {
-        s: function() {
-            return this.$route.query.s;
-        }
-    },
-    methods: {
-        replaceBrokenImg() {
-            this.list.forEach((n, i) => {
-                let img = new Image();
-                img.onerror = function() {
-                    n.imgIsBroken = true;
-                    n.cover = "/img/blog/article-nopic.jpeg";
-                };
-                img.src = n.cover;
-            });
-        },
-        deleteArticle() {
-            this.$confirm("确认删除此文章？")
-                .then(() => {
-                    // TODO: 删除API
-                })
-                .catch(() => {});
-        },
-        load(page) {
-            axios
-                .post(
-                    "https://api.yuyehack.cn/blog/article/get_articlelist.php",
-                    {
-                        s: this.s,
-                        page: page,
-                        pageSize: 8
-                    }
-                )
-                .then(res => {
-                    let newList = res.data.list;
-                    this.list = [...this.list, ...newList];
-                    this.isLoad = true;
-                    this.currentPage++;
-                    this.replaceBrokenImg();
-                });
-        },
-        eventListen() {
-            let ele = document.documentElement;
-            if (ele.clientWidth > 768) {
-                if (ele.scrollHeight - ele.scrollTop - ele.clientHeight < 30) {
-                    if (this.currentPage < this.totalPages) {
-                        if (this.isLoad) {
-                            this.isLoad = false;
-                            this.load(this.currentPage + 1);
-                        }
-                    }
-                }
-            }
-        }
-    },
-    mounted() {
-        this.replaceBrokenImg();
-        let vm = this;
-        let ele = document.documentElement;
-        document
-            .querySelectorAll(".container")[0]
-            .addEventListener("scroll", vm.eventListen);
-    },
-    beforeDestroy() {
-        let vm = this;
-        document
-            .querySelectorAll(".container")[0]
-            .removeEventListener("scroll", vm.eventListen);
-    },
-    watch: {
-        "$route.query": function(to, from) {
-            axios
-                .post(
-                    "https://api.yuyehack.cn/blog/article/get_articlelist.php",
-                    {
-                        s: this.$route.query.s,
-                        page: 1,
-                        pageSize: 8
-                    }
-                )
-                .then(res => {
-                    this.list = res.data.list;
-                    this.totalPages = res.data.totalPages;
-                });
-        }
+  layout: "admin",
+  async asyncData({ query }) {
+    function getArticle() {
+      return axios
+        .post("https://api.yuyehack.cn/blog/article/get_articlelist.php", {
+          s: query.s,
+          page: 1,
+          pageSize: 8
+        })
+        .then(res => {
+          return {
+            list: res.data.list,
+            totalPages: res.data.totalPages
+          };
+        })
+        .catch(e => {
+          // context.error({ statusCode: 404, message: "出错啦" });
+          return { list: [{ title: "出错啦", info: "请检查接口" }] };
+        });
     }
+    let data = await getArticle();
+    return data;
+  },
+  data() {
+    return {
+      currentPage: 1,
+      isLoad: true
+    };
+  },
+
+  computed: {
+    s: function() {
+      return this.$route.query.s;
+    }
+  },
+  methods: {
+    replaceBrokenImg() {
+      this.list.forEach((n, i) => {
+        let img = new Image();
+        img.onerror = function() {
+          n.imgIsBroken = true;
+          n.cover = "/img/blog/article-nopic.jpeg";
+        };
+        img.src = n.cover;
+      });
+    },
+    deleteArticle() {
+      this.$confirm("确认删除此文章？")
+        .then(() => {
+          // TODO: 删除API
+        })
+        .catch(() => {});
+    },
+    load(page) {
+      axios
+        .post("https://api.yuyehack.cn/blog/article/get_articlelist.php", {
+          s: this.s,
+          page: page,
+          pageSize: 8
+        })
+        .then(res => {
+          let newList = res.data.list;
+          this.list = [...this.list, ...newList];
+          this.isLoad = true;
+          this.currentPage++;
+          this.replaceBrokenImg();
+        });
+    },
+    eventListen() {
+      let ele = document.documentElement;
+      if (ele.clientWidth > 768) {
+        if (ele.scrollHeight - ele.scrollTop - ele.clientHeight < 30) {
+          if (this.currentPage < this.totalPages) {
+            if (this.isLoad) {
+              this.isLoad = false;
+              this.load(this.currentPage + 1);
+            }
+          }
+        }
+      }
+    }
+  },
+  mounted() {
+    this.replaceBrokenImg();
+    let vm = this;
+    let ele = document.documentElement;
+    document
+      .querySelectorAll(".container")[0]
+      .addEventListener("scroll", vm.eventListen);
+  },
+  beforeDestroy() {
+    let vm = this;
+    document
+      .querySelectorAll(".container")[0]
+      .removeEventListener("scroll", vm.eventListen);
+  },
+  watch: {
+    "$route.query": function(to, from) {
+      axios
+        .post("https://api.yuyehack.cn/blog/article/get_articlelist.php", {
+          s: this.$route.query.s,
+          page: 1,
+          pageSize: 8
+        })
+        .then(res => {
+          this.list = res.data.list;
+          this.totalPages = res.data.totalPages;
+        });
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
 .ht {
-    height: 56px;
-    line-height: 56px;
-    padding: 0 30px;
-    border-bottom: 1px solid #eee;
-    box-shadow: 0 2px 5px -1px rgba(0, 0, 0, 0.05);
-    background-image: linear-gradient(
-        rgba(200, 200, 200, 0),
-        rgba(200, 200, 200, 0.12)
-    );
-    color: rgba(0, 0, 0, 0.4);
-    .add {
-        color: #20a0ff;
-        margin-left: 20px;
-    }
+  height: 56px;
+  line-height: 56px;
+  padding: 0 30px;
+  border-bottom: 1px solid #eee;
+  box-shadow: 0 2px 5px -1px rgba(0, 0, 0, 0.05);
+  background-image: linear-gradient(
+    rgba(200, 200, 200, 0),
+    rgba(200, 200, 200, 0.12)
+  );
+  color: rgba(0, 0, 0, 0.4);
+  .add {
+    color: #20a0ff;
+    margin-left: 20px;
+  }
 }
 .content {
-    padding: 0 30px;
-    .list {
-        padding: 30px 0;
-        border-bottom: 1px solid #eee;
-        a {
-            color: #333;
-            text-decoration: none;
-        }
-        h2 {
-            font-size: 24px;
-            line-height: 30px;
-            margin: 0 0 10px;
-            color: #444;
-            font-weight: 400;
-
-            a {
-                &:hover {
-                    color: #3cafff;
-                }
-            }
-        }
-        .edit {
-            color: #20a0ff;
-            font-size: 16px;
-        }
-        .del {
-            color: red;
-            font-size: 16px;
-            margin-left: 10px;
-            cursor: pointer;
-        }
-        .info {
-            min-height: 80px;
-            font-size: 14px;
-            margin: 0;
-            color: #696969;
-            line-height: 24px;
-            letter-spacing: 1px;
-            display: block;
-            img {
-                max-width: 180px;
-                max-height: 80px;
-                float: right;
-                margin-left: 10px;
-                border-radius: 4px;
-            }
-        }
-
-        .meta {
-            margin-top: 10px;
-            color: #999;
-            .tag {
-                margin-left: 10px;
-                font-size: 14px;
-            }
-        }
+  padding: 0 30px;
+  .list {
+    padding: 30px 0;
+    border-bottom: 1px solid #eee;
+    a {
+      color: #333;
+      text-decoration: none;
     }
+    h2 {
+      font-size: 24px;
+      line-height: 30px;
+      margin: 0 0 10px;
+      color: #444;
+      font-weight: 400;
+
+      a {
+        &:hover {
+          color: #3cafff;
+        }
+      }
+    }
+    .edit {
+      color: #20a0ff;
+      font-size: 16px;
+    }
+    .del {
+      color: red;
+      font-size: 16px;
+      margin-left: 10px;
+      cursor: pointer;
+    }
+    .info {
+      min-height: 80px;
+      font-size: 14px;
+      margin: 0;
+      color: #696969;
+      line-height: 24px;
+      letter-spacing: 1px;
+      display: block;
+      img {
+        max-width: 180px;
+        max-height: 80px;
+        float: right;
+        margin-left: 10px;
+        border-radius: 4px;
+      }
+    }
+
+    .meta {
+      margin-top: 10px;
+      color: #999;
+      .tag {
+        margin-left: 10px;
+        font-size: 14px;
+      }
+    }
+  }
 }
 </style>
 
