5ad062164338d05d55b395aef9188fd06ebcf61c
chenyeah <yuyehack@gmail.com>
2018-06-01 19:44:17 +0800

    fixed randomimg

--- components/ad.vue
+++ components/ad.vue
@@ -1,14 +1,25 @@
 <template>
-    <div class="cont">
-        <img src="https://source.unsplash.com/random/400x300" alt="">
-    </div>
+  <div class="cont">
+    <img :src="url" alt="">
+  </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            url: "http://api.yuyehack.cn/blog/baiduimg.php"
+        };
+    }
+};
+</script>
+
 <style lang="scss" scoped>
 .cont {
     background: #fff;
     border-radius: 10px;
-    box-shadow: 0 0 0 1px rgba(0, 0, 0, .02), 0 4px 10px rgba(0, 0, 0, .06);
-    img{
+    box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.02), 0 4px 10px rgba(0, 0, 0, 0.06);
+    img {
         display: block;
         width: 100%;
         border-radius: 10px;
