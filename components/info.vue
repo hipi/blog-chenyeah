dcbe8bd838e4a728d869c394101a42221ea44c98
chenyeah <yuyehack@gmail.com>
2018-06-04 17:18:18 +0800

    remove admin entry

--- components/info.vue
+++ components/info.vue
@@ -3,7 +3,7 @@
     <i @click="isMusic=!isMusic" class="music iconfont icon-music"></i>
     <audio v-if="isMusic" loop autoplay src="/music/bg.mp3"></audio>
     <div class="head">
-      <nuxt-link to='/admin'>
+      <nuxt-link to='/blog'>
         <img :class="{play:isMusic}" src="https://www.gravatar.com/avatar/40206d98ff6e85da457c341e4a4ea437" alt="">
       </nuxt-link>
     </div>
